9e88d93b6a0a45d6761cfc8d20af5b4a21dd2cea
Silvestru Radu-Gabriel <silvestruradugabriel@gmail.com>
2022-04-30 12:51:44 +0300

    Adding Refund and Loan.

--- src/main/java/ro/sda/finalProject/carRental/repository/LoanRepository.java
+++ src/main/java/ro/sda/finalProject/carRental/repository/LoanRepository.java
@@ -1,8 +1,8 @@
 package ro.sda.finalProject.carRental.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ro.sda.finalProject.carRental.entities.Rental;
+import ro.sda.finalProject.carRental.entities.Loan;
+
 @Repository
-public interface RentalRepository extends JpaRepository<Rental, Integer> {
+public interface LoanRepository extends JpaRepository<Loan, Integer> {
 }
--- src/main/java/ro/sda/finalProject/carRental/repository/RefundRepository.java
+++ src/main/java/ro/sda/finalProject/carRental/repository/RefundRepository.java
@@ -1,8 +1,8 @@
 package ro.sda.finalProject.carRental.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ro.sda.finalProject.carRental.entities.Rental;
+import ro.sda.finalProject.carRental.entities.Refund;
+
 @Repository
-public interface RentalRepository extends JpaRepository<Rental, Integer> {
+public interface RefundRepository extends JpaRepository<Refund, Integer> {
 }
